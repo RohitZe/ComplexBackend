181eadb35bb969a1e16f9f70ccf775e4383cd403
RohitZe <rohithsharmaa105@gmail.com>
2025-01-11 17:46:04 +0530

    add user video models and custom api error classes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 //require('dotenv').config({path:'./env'})
+import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from "./db/index.js";
 
+const app=express();
 
 dotenv.config({
     path:'./env'
 })
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000 ,()=>{
+        console.log(`Server is running :${process.env.PORT}`);
+    });
+})
+.catch((err)=>{
+    console.log("MongoDb connection failed re baba",err);
+});
