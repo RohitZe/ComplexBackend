562cdd0f14153c9f026ed44f75366aa3a7e320fc
RohitZe <rohithsharmaa105@gmail.com>
2025-01-11 22:52:39 +0530

    middleware done

--- src/utils/apiError.js
+++ src/utils/apiError.js
@@ -2,8 +2,8 @@ class ApiError extends Error{
     constructor(
         statusCode,
         message="something went wrong",
-        error=[],
-        statck="",
+        errors=[],
+        stack="",
     ){
         super(message)
         this.statusCode=statusCode
@@ -11,9 +11,9 @@ class ApiError extends Error{
         this.message=message
         this.errors=errors
         
-        if(statck)
+        if(stack)
         {
-         this.stack=statck
+         this.stack=stack
         }else
         {
          Error.captureStackTrace(this,this.constructor)
