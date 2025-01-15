084d68ee3bac80947768a7e7551f10574a25a047
RohitZe <rohithsharmaa105@gmail.com>
2025-01-15 19:48:01 +0530

    user registering logic served

--- src/app.js
+++ src/app.js
@@ -1,53 +1,24 @@
-// import express from "express";
-// import cookieParser from "cookie-parser";
-// import cors from "cors"
-
-// const app=express();
-// app.use(cors({
-//     origin:process.env.CROSS_ORIGIN,
-//     Credential:true
-// }))
-
-
-// app.use(express.json({limit:"16kb"}))
-// app.use(express.urlencoded({extended: true,limit:"16kb"}))
-// app.use(express.static("public"))
-// app.use(express.cookieParser())
-// console.log("step-1");
-// //routes import
-// import userRouter from "./routes/user.route.js"
-
-// //routes decleration
-// //app.use('/api/v1/users',userRouter)
-
-// app.get("/", (req, res, next) => {
-//     return res.json({
-//       code: 200,
-//       message: "Server is Up"
-//     })
-//   })
-// console.log("step-2");
-import express from "express"
+import express from "express";
+import cookieParser from "cookie-parser";
 import cors from "cors"
-import cookieParser from "cookie-parser"
 
-const app = express()
+const app=express();
 app.use(cors({
-    origin:process.env.CORS_ORIGIN,
-    credentials:true
+    origin:process.env.CROSS_ORIGIN,
+    Credential:true
 }))
 
+
 app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extended:true,limit:"16kb"}))
+app.use(express.urlencoded({extended: true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+//routes import
+import userRouter from "./routes/user.route.js"
 
-app.get("/rohit", (req, res, next) => {
-    return res.json({
-      code: 200,
-      message: "Server is up"
-    })
-  })
+app.use('/api/v1/users',userRouter)
+
+
 
 export {app}
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,15 +1,77 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/apiError.js";
+import {User} from "../models/user.model.js";
+import {uploadOnCloudinary} from "../utils/cloudinary.js";
+import {Apiresponse} from "../utils/apiResponse.js"
+
+
+
 
 const registerUser=asyncHandler( async (req,res)=>{
+ //step-1 get data from frontend 
+   
+  const {fullname,email,password,username}=req.body
+  console.log("email",email);
+ //validation
 
-    res.status(200).json(
-        {
-        "message":"ok",
-        }
-)
+ if([fullname,email,password,username].some((field)=>
+    field?.trim()===""))
+ {
+  throw new ApiError(400,"All fields are required")
+ }
+
+//checking if user already exist
+ const existedUser=User.findOne(
+    {
+    $or:[{email}, {username}]
+    })
+
+if(existedUser)
+{
+    throw new ApiError(400,"Username and email already existed")
+}
+
+const avatarLocalPath=req.files?.avatar[0]?.path;
+const coverImageLocalPath=req.files?.coverImage[0]?.path;
+
+if(!avatarLocalPath)
+{
+    throw new ApiError(400,"Avatar is required")
+}
+
+const avatar=await uploadOnCloudinary(avatarLocalPath);
+const coverImage=await uploadOnCloudinary(coverImageLocalPath);
+
+if(!avatar)
+{
+    throw new ApiError(400,"Avatar is required")   
+}
+
+const user= await User.create({
+    fullname,
+    avatar:avatar.url,
+    coverImage:coverImage?.url||"",
+    email,
+    password,
+    username:username.toLowerCase()
 })
 
-export {
-    registerUser,
+const createdUser=await User.findById(user._id).select(
+    "-password -refreshToken"
+)
+
+if(!createdUser)
+{
+    throw new ApiError(500,"something went wrong while registering the user")
 }
+
+ return res.status(201).json(
+    new Apiresponse(200,createdUser,"User registered successfully")
+ )
+
+})
+
+export { registerUser}
+
+
 
